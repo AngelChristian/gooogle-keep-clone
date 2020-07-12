fad7c1bd8d5bbce0eb2efff0b794a4b75d61a051
Angel Christian <christianangel@gmail.com>
2020-07-13 00:44:51 +0530

    font issue fix

---
+++ build/precache-manifest.dfdcbefc543cc9066fc7cd6896b499e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d116d53238213db4e2b185a2bccd2546",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f3c7ee552ebf69cb0e9",
+    "url": "/static/js/2.d5d55834.chunk.js"
+  },
+  {
+    "revision": "1260a01290ac0c0454e9a7627323041c",
+    "url": "/static/js/2.d5d55834.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b00986ed82356af53107",
+    "url": "/static/js/main.e28c40f7.chunk.js"
+  },
+  {
+    "revision": "ae75c5421005647ef51e",
+    "url": "/static/js/runtime-main.802a07ba.js"
+  }
+]);
